5b991782ad88aa87ae9256be420a152a18c82ac6
xiang9.li <xiang9.li@ly.com>
2023-02-19 20:35:56 +0800

    修改路劲

--- lua/lazyvim/plugins/core.lua
+++ lua/lazyvim/plugins/core.lua
@@ -2,5 +2,5 @@ require("lazyvim.config").init()
 
 return {
   { "folke/lazy.nvim", version = "*" },
-  { "leecore398/LazyVim", priority = 10000, lazy = false, config = true, version = "*" },
+  { "leecode398/LazyVim", priority = 10000, lazy = false, config = true, version = "*" },
 }
